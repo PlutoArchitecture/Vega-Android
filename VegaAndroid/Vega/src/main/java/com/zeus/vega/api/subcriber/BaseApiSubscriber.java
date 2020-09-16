235dad07a2c3bb3f2fa22f63a2573c2a45b82805
戴统民 <minggo.dtm@nonolive.com>
2020-09-16 10:04:56 +0800

    feat：网络层--返回结果泛化代码简化。

--- VegaAndroid/Vega/src/main/java/com/zeus/vega/api/subcriber/BaseApiSubscriber.java
+++ VegaAndroid/Vega/src/main/java/com/zeus/vega/api/subcriber/BaseApiSubscriber.java
@@ -18,7 +18,7 @@ public abstract class BaseApiSubscriber<T> extends DisposableObserver<Result<T>>
 
     @Override
     public final void onComplete() {
-        if (!isDisposed()){
+        if (!isDisposed()) {
             dispose();
         }
     }
@@ -27,7 +27,7 @@ public abstract class BaseApiSubscriber<T> extends DisposableObserver<Result<T>>
     public final void onError(Throwable e) {
         if (isDisposed()) {
             return;
-        }else{
+        } else {
             dispose();
         }
 
@@ -40,14 +40,14 @@ public abstract class BaseApiSubscriber<T> extends DisposableObserver<Result<T>>
     public final void onNext(Result<T> result) {
         if (isDisposed()) {
             return;
-        }else{
+        } else {
             dispose();
         }
 
         if (result != null) {
-            if (isT2Result()){
+            if (isT2Result()) {
                 onApiNext((T) result);
-            }else{
+            } else {
                 onApiNext(result.content);
             }
         }
@@ -55,21 +55,20 @@ public abstract class BaseApiSubscriber<T> extends DisposableObserver<Result<T>>
 
     /**
      * 通过层层剥离泛型层级来判别Result<Result>，但暂时不能用来判断出List或其他装载类型（可以考虑获取typename的String类型来判别）
+     *
      * @return 是否需要返回最外层Result
      */
-    private boolean isT2Result(){
+    private boolean isT2Result() {
         Type type = getClass().getGenericSuperclass();
-        if (!(type instanceof Class<?>)) {
-            if (type instanceof ParameterizedType) {
-                Type actualTypeArgument = ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
-                if (actualTypeArgument instanceof ParameterizedType) {
-                    return false;
-                }else if (actualTypeArgument instanceof  Class){
-                    Class<T> tClass = (Class<T>) actualTypeArgument;
-                    return tClass == Result.class;
-                }else {
-                    throw new InvalidParameterException("VegaHttpClient isn't supported this Type -> "+type.toString());
-                }
+        if (type instanceof ParameterizedType) {
+            Type actualTypeArgument = ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+            if (actualTypeArgument instanceof ParameterizedType) {
+                return false;
+            } else if (actualTypeArgument instanceof Class) {
+                Class<T> tClass = (Class<T>) actualTypeArgument;
+                return tClass == Result.class;
+            } else {
+                throw new InvalidParameterException("VegaHttpClient isn't supported this Type -> " + type.toString());
             }
         }
         return false;
@@ -77,6 +76,7 @@ public abstract class BaseApiSubscriber<T> extends DisposableObserver<Result<T>>
 
 
     protected abstract void onApiNext(T t);
+
     protected abstract void onApiError(ResponseThrowable responseThrowable);
 
 }
